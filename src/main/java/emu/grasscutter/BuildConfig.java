fd05f713af08a16cf5de77fc01951b5b3deb0fce
YuYeoreum <legendyeoreum@gmail.com>
2022-08-28 20:21:31 +0900

    다른파일 추가

---
+++ src/main/java/emu/grasscutter/BuildConfig.java
@@ -0,0 +1,7 @@
+
+        package emu.grasscutter;
+        public class BuildConfig {
+                public static final String VERSION = "1.2.0";
+                public static final String GIT_HASH = "";
+        }
+    
